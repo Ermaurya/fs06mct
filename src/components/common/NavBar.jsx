503c786b48871abe5c1d426e3346cd0f8fdcd8a6
shubham maurya <cs.shubhammaurya@gmail.com>
2023-09-25 16:06:13 +0530

    first commit

---
+++ src/components/common/NavBar.jsx
@@ -0,0 +1,18 @@
+
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import logo from './logo.svg';
+const NavBar = () => {
+
+    return (
+        <Menu borderless fixed="top">
+            <Menu.Item>
+                <img src={logo} alt="logo" style={{ width: 150 }} />
+            </Menu.Item>
+           <Menu.Item name="Home" as={Link} to="/" /> 
+           <Menu.Item name="Recipes" as={Link} to="/recipes" /> 
+        </Menu>
+    )
+}
+
+export default NavBar;
